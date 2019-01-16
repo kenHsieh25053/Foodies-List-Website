3bda0f68aa3cc4faa5e76d6f04ecb4a41837cdaf
DESKTOP-A1UNEF0\KW198 <kw1984@livemail.tw>
2019-01-16 22:33:13 +0800

    fix django and vue intergation issue

--- foodies/settings/common.py
+++ foodies/settings/common.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../frontend/dist'),
+    # os.path.join(BASE_DIR, '../public'),
 )
 
 WEBPACK_LOADER = {
